dc9cadc018372a4d07cdb8af93c18c04f24073cc
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-06 13:50:46 +0200

    Initial commit

---
+++ TurnDirectorBugFixes/TurnDirectorBugFixes.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using nl.flukeyfiddler.bt.TurnDirectorBugFixes.Util;
+using nl.flukeyfiddler.bt.Utils.Logger;
+using System.IO;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.bt.TurnDirectorBugFixes
+{
+    public class TurnDirectorBugFixes
+    {
+        public static void Init(string modDirectory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("nl.flukeyfiddler.bt.TurnDirectorBugFixes");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+
+            Logger.SetLogFilePath(new LogFilePath(Path.Combine(modDirectory, "Log.txt")));
+            Logger.GameStarted();
+        }
+    }
+}
---
+++ TurnDirectorBugFixes/Utils/Logger.cs
@@ -0,0 +1,41 @@
+﻿using nl.flukeyfiddler.bt.Utils.Logger;
+using System;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.bt.TurnDirectorBugFixes.Util
+{
+    public static class Logger
+    {
+        private static LogFilePath logFilePath;
+        
+        public static void SetLogFilePath(LogFilePath logFilePath)
+        {
+            Logger.logFilePath = logFilePath;
+        }
+
+        public static void Error(Exception ex, MethodBase caller = null)
+        {
+            LoggerUtil.Error(logFilePath, ex, caller);
+        }
+
+        public static void Line(string line, MethodBase caller = null)
+        {
+            LoggerUtil.Line(logFilePath, line, caller);
+        }
+
+        public static void Minimal(string line)
+        {
+            LoggerUtil.Minimal(logFilePath, line);
+        }
+
+        public static void Block(string[] lines, MethodBase caller = null)
+        {
+            LoggerUtil.Block(logFilePath, lines, caller);
+        }
+
+        public static void GameStarted()
+        {
+            LoggerUtil.GameStarted(logFilePath);
+        }
+    }
+}
